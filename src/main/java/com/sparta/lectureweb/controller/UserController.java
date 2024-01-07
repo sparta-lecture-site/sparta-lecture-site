e55ff5a46c2fa6911a0d755072fc5c9af6636256
mum <mumum3521@naver.com>
2024-01-07 22:18:55 +0900

    Feat : 로그인 기능 추가

--- src/main/java/com/sparta/lectureweb/controller/UserController.java
+++ src/main/java/com/sparta/lectureweb/controller/UserController.java
@@ -2,6 +2,8 @@ package com.sparta.lectureweb.controller;
 
 import com.sparta.lectureweb.domain.dto.UserDto;
 import com.sparta.lectureweb.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<String> createUser(@RequestBody UserDto requestDto) {
+    public ResponseEntity<Void> createUser(@RequestBody UserDto requestDto) {
         userService.createUser(requestDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<Void> login(@RequestBody UserDto requestDto, HttpServletResponse response) {
+        userService.login(requestDto, response);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/sparta/lectureweb/service/UserService.java
+++ src/main/java/com/sparta/lectureweb/service/UserService.java
@@ -3,11 +3,15 @@ package com.sparta.lectureweb.service;
 import com.sparta.lectureweb.domain.dto.UserDto;
 import com.sparta.lectureweb.domain.entity.User;
 import com.sparta.lectureweb.repository.UserRepository;
+import com.sparta.lectureweb.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ import org.springframework.validation.annotation.Validated;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
     @Transactional
     public void createUser(@Valid UserDto requestDto) {
 
@@ -28,5 +33,17 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public void login(UserDto requestDto, HttpServletResponse response) {
+
+        User user = userRepository.findByEmail(requestDto.getEmail()).orElseThrow();
+
+        if(!user.getPassword().equals(requestDto.getPassword())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+        }
+
+        String token = jwtUtil.createToken(user.getEmail(), user.getAuthority());
+        jwtUtil.addJwtToCookie(token,response);
     }
 }
